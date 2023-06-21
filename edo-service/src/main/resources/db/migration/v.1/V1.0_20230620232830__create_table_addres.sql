9e33f3494970b8dd2d3e5cbf5ba24442b7f08760
Eugene Strelecky <Macfliner@gmail.com>
2023-06-21 16:26:36 +0300

    Fix bug - rename migration script for address table.

--- edo-service/src/main/resources/db/migration/v.1/V1.0_20230620232830__create_table_addres.sql
+++ edo-service/src/main/resources/db/migration/v.1/V1.0_20230620232830__create_table_addres.sql
@@ -1,17 +1,18 @@
 CREATE SCHEMA IF NOT EXISTS edo;
-CREATE TABLE IF NOT EXISTS edo.address (
-                                        id BIGSERIAL PRIMARY KEY,
-                                        full_address VARCHAR(500),
-                                        street VARCHAR(50),
-                                        house VARCHAR(50),
-                                        postcode VARCHAR(50),
-                                        housing VARCHAR(50),
-                                        building VARCHAR(50),
-                                        city VARCHAR(50),
-                                        region VARCHAR(50),
-                                        country VARCHAR(50),
-                                        flat VARCHAR(50)
-                                        );
+CREATE TABLE IF NOT EXISTS edo.address
+(
+    id           BIGSERIAL PRIMARY KEY,
+    full_address VARCHAR(500),
+    street       VARCHAR(50),
+    house        VARCHAR(50),
+    postcode     VARCHAR(50),
+    housing      VARCHAR(50),
+    building     VARCHAR(50),
+    city         VARCHAR(50),
+    region       VARCHAR(50),
+    country      VARCHAR(50),
+    flat         VARCHAR(50)
+);
 COMMENT ON COLUMN edo.address.id IS 'Уникальный идентификатор адреса';
 COMMENT ON COLUMN edo.address.full_address IS 'Полный адрес';
 COMMENT ON COLUMN edo.address.street IS 'Название улицы';
