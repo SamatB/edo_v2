f60f2af575fed64ae2a8cb5276045b10cf0936b4
Shaydulin Erik <mikestrat0xabcdef@gmail.com>
2023-06-21 20:10:57 +0500

    removed from the migration file, creating an edo schema.

--- edo-service/src/main/resources/db/migration/v.1/V1.0__230619044601_create_appeal.sql
+++ edo-service/src/main/resources/db/migration/v.1/V1.0__230619044601_create_appeal.sql
@@ -1,6 +1,4 @@
-CREATE SCHEMA IF NOT EXISTS edo;
-
-CREATE TABLE IF NOT EXISTS edo.appeal (
+CREATE TABLE IF NOT EXISTS appeal (
                                           id BIGSERIAL PRIMARY KEY,
                                           creation_date TIMESTAMP NOT NULL,
                                           archived_date TIMESTAMP,
@@ -8,8 +6,8 @@ CREATE TABLE IF NOT EXISTS edo.appeal (
                                           annotation VARCHAR(1000)
 );
 
-comment on column edo.appeal.id is 'Идентификатор обращения';
-comment on column edo.appeal.creation_date is 'Дата создания';
-comment on column edo.appeal.archived_date is 'Дата архивации';
-comment on column edo.appeal.number is 'Номер обращения';
-comment on column edo.appeal.annotation is 'Описание обращения';
+comment on column appeal.id is 'Идентификатор обращения';
+comment on column appeal.creation_date is 'Дата создания';
+comment on column appeal.archived_date is 'Дата архивации';
+comment on column appeal.number is 'Номер обращения';
+comment on column appeal.annotation is 'Описание обращения';
