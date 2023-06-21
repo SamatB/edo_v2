17e867193591f670fe5268e448f878157b4fa0a4
Eugene Strelecky <Macfliner@gmail.com>
2023-06-21 18:22:49 +0300

    Removed the creation of a new schema from the migration script.

--- edo-service/src/main/resources/db/migration/v.1/V1.0_20230620232830__create_table_addres.sql
+++ edo-service/src/main/resources/db/migration/v.1/V1.0_20230620232830__create_table_addres.sql
@@ -1,5 +1,4 @@
-CREATE SCHEMA IF NOT EXISTS edo;
-CREATE TABLE IF NOT EXISTS edo.address
+CREATE TABLE IF NOT EXISTS address
 (
     id           BIGSERIAL PRIMARY KEY,
     full_address VARCHAR(500),
@@ -13,14 +12,14 @@ CREATE TABLE IF NOT EXISTS edo.address
     country      VARCHAR(50),
     flat         VARCHAR(50)
 );
-COMMENT ON COLUMN edo.address.id IS 'Уникальный идентификатор адреса';
-COMMENT ON COLUMN edo.address.full_address IS 'Полный адрес';
-COMMENT ON COLUMN edo.address.street IS 'Название улицы';
-COMMENT ON COLUMN edo.address.house IS 'Номер дома';
-COMMENT ON COLUMN edo.address.postcode IS 'Почтовый индекс';
-COMMENT ON COLUMN edo.address.housing IS 'Корпус здания';
-COMMENT ON COLUMN edo.address.building IS 'Строение здания';
-COMMENT ON COLUMN edo.address.city IS 'Город';
-COMMENT ON COLUMN edo.address.region IS 'Регион или область';
-COMMENT ON COLUMN edo.address.country IS 'Страна';
-COMMENT ON COLUMN edo.address.flat IS 'Номер квартиры';
+COMMENT ON COLUMN address.id IS 'Уникальный идентификатор адреса';
+COMMENT ON COLUMN address.full_address IS 'Полный адрес';
+COMMENT ON COLUMN address.street IS 'Название улицы';
+COMMENT ON COLUMN address.house IS 'Номер дома';
+COMMENT ON COLUMN address.postcode IS 'Почтовый индекс';
+COMMENT ON COLUMN address.housing IS 'Корпус здания';
+COMMENT ON COLUMN address.building IS 'Строение здания';
+COMMENT ON COLUMN address.city IS 'Город';
+COMMENT ON COLUMN address.region IS 'Регион или область';
+COMMENT ON COLUMN address.country IS 'Страна';
+COMMENT ON COLUMN address.flat IS 'Номер квартиры';
