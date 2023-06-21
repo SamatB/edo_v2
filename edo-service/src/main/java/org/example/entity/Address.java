9d77e5a130b4dcf0e3a6184dae41a587d4023f02
Eugene Strelecky <Macfliner@gmail.com>
2023-06-21 15:18:38 +0300

    Fix import in Address entity class.

--- edo-service/src/main/java/org/example/entity/Address.java
+++ edo-service/src/main/java/org/example/entity/Address.java
@@ -3,7 +3,11 @@ package org.example.entity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
 /**
  * Класс, представляющий адрес.
