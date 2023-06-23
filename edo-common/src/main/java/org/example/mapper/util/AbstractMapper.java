8868d1d29c466347d5b6797c6bccd28a71a98c0b
Nedelko Dmitry <dmitry30reg@mail.ru>
2023-06-23 20:12:12 +0400

    pom.xml(edo) wqs refactored. New annotations in AbstractMapper class was added and methods names was changed.

--- edo-common/src/main/java/org/example/mapper/util/AbstractMapper.java
+++ edo-common/src/main/java/org/example/mapper/util/AbstractMapper.java
@@ -6,35 +6,39 @@
 
 package org.example.mapper.util;
 
-import org.mapstruct.Mapper;
+import org.mapstruct.InheritInverseConfiguration;
+import org.mapstruct.MapperConfig;
+import org.mapstruct.MappingTarget;
 
 import java.util.List;
 
-@Mapper
+@MapperConfig(componentModel = "spring")
 public abstract class AbstractMapper<S, T> {
 
     /**
      * Метод преобразует сущность JPA в DTO объект
      */
-    public abstract T toDto(S entity);
+    @InheritInverseConfiguration
+    public abstract T EntityToDto(S entity);
 
     /**
      * Метод преобразует DTO объект в сущность JPA
      */
-    public abstract S toEntity(T dto);
+    public abstract S DtoToEntity(T dto);
 
     /**
      * Метод обновляет поля сущности JPA на основе переданного DTO объекта
      */
-    public abstract void updateEntity(T dto, S entity);
+    public abstract void updateEntity(T dto, @MappingTarget S entity);
 
     /**
      * Метод преобразует список сущностей JPA в список сущностей DTO
      */
-    public abstract List<T> toDtoList(List<S> entities);
+    @InheritInverseConfiguration
+    public abstract List<T> EntityListtoDtoList(List<S> entities);
 
     /**
      * Метод преобразует список объектов DTO в список сущностей JPA
      */
-    public abstract List<S> toEntityList(List<T> dtos);
+    public abstract List<S> DtoListtoEntityList(List<T> dtos);
 }
