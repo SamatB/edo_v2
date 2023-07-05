e59f22c3edae266629ee6e436609fa37e1c5b080
Nedelko Dmitry <dmitry30reg@mail.ru>
2023-07-05 11:01:45 +0400

    AbstractMapper transformed from abstract class to interface.

--- edo-common/src/main/java/org/example/mapper/util/AbstractMapper.java
+++ edo-common/src/main/java/org/example/mapper/util/AbstractMapper.java
@@ -1,6 +1,6 @@
 /**
- * Абстрактынй класс маппера для преобразования одного типа данных в другой
- * От данного класса будут наследоваться другие мапперы
+ * Интерфейс маппера для преобразования одного типа данных в другой
+ * Данный интерфейс будут реализовывать другие мапперы
  */
 
 
@@ -13,34 +13,34 @@ import org.mapstruct.MappingTarget;
 import java.util.List;
 
 @MapperConfig(componentModel = "spring")
-public abstract class AbstractMapper<Entity, Dto> {
+public interface AbstractMapper<Entity, Dto> {
 
     /**
      * Метод преобразует сущность JPA в DTO объект
      */
 
-    public abstract Dto EntityToDto(Entity entity);
+    Dto EntityToDto(Entity entity);
 
     /**
      * Метод преобразует DTO объект в сущность JPA
      */
     @InheritConfiguration(name = "EntityToDto")
-    public abstract Entity DtoToEntity(Dto dto);
+    Entity DtoToEntity(Dto dto);
 
     /**
      * Метод обновляет поля сущности JPA на основе переданного DTO объекта
      */
-    public abstract void updateEntity(Dto dto, @MappingTarget Entity entity);
+    void updateEntity(Dto dto, @MappingTarget Entity entity);
 
     /**
      * Метод преобразует список сущностей JPA в список сущностей DTO
      */
 
-    public abstract List<Dto> EntityListToDtoList(List<Entity> entities);
+    List<Dto> EntityListToDtoList(List<Entity> entities);
 
     /**
      * Метод преобразует список объектов DTO в список сущностей JPA
      */
     @InheritConfiguration(name = "EntityListToDtoList")
-    public abstract List<Entity> DtoListToEntityList(List<Dto> dtos);
+    List<Entity> DtoListToEntityList(List<Dto> dtos);
 }
