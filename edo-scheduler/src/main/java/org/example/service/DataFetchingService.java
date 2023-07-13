2b577087da74860368e83508ab2ce083cab8277f
Eugene Strelecky <Macfliner@gmail.com>
2023-07-13 17:01:23 +0300

    Task 24. Сhanged return to Stream in methods of DataFetchingService class.

--- edo-scheduler/src/main/java/org/example/service/DataFetchingService.java
+++ edo-scheduler/src/main/java/org/example/service/DataFetchingService.java
@@ -1,6 +1,5 @@
 package org.example.service;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.log4j.Log4j2;
 import org.example.dto.AddressDto;
 import org.example.dto.DepartmentDto;
@@ -15,10 +14,8 @@ import org.springframework.web.client.RestTemplate;
 
 import java.time.LocalDate;
 import java.time.ZonedDateTime;
-import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * Сервис для получения данных и преобразования их в объекты DTO.
@@ -30,10 +27,6 @@ public class DataFetchingService {
      * Клиент RestTemplate для выполнения HTTP-запросов к внешнему API.
      */
     private final RestTemplate restTemplate;
-    /**
-     * Объект ObjectMapper для преобразования JSON-строк в объекты Java и наоборот.
-     */
-    private final ObjectMapper objectMapper;
     /**
      * URL адрес внешнего хранилища данных.
      */
@@ -49,7 +42,6 @@ public class DataFetchingService {
     public DataFetchingService(@Value("${external.storage.url}") String externalStorageUrl) {
         this.externalStorageUrl = externalStorageUrl;
         this.restTemplate = new RestTemplate();
-        this.objectMapper = new ObjectMapper();
     }
 
     /**
@@ -87,35 +79,35 @@ public class DataFetchingService {
      * @return Список объектов EmployeeDto.
      */
     private List<EmployeeDto> mapToEmployeeDto(List<ExternalData> externalDataList) {
-        List<EmployeeDto> employeeDtoList = new ArrayList<>();
-        for (ExternalData externalData : externalDataList) {
-            ExternalData.Location location = externalData.getLocation();
-            ExternalData.Name name = externalData.getName();
-            String externalId = String.valueOf(externalData.getId());
-            AddressDto addressDto = new AddressDto();
-            addressDto.setFullAddress(location.toString());
-            addressDto.setStreet(location.getStreet().getName());
-            addressDto.setHouse(location.getStreet().getNumber());
-            addressDto.setIndex(location.getPostcode());
-            addressDto.setCity(location.getCity());
-            addressDto.setRegion(location.getState());
-            addressDto.setCountry(location.getCountry());
-            EmployeeDto employeeDto = new EmployeeDto();
-            employeeDto.setExternalId(externalId);
-            employeeDto.setFirstName(name.getFirst());
-            employeeDto.setLastName(name.getLast());
-            employeeDto.setMiddleName(name.getMiddle());
-            employeeDto.setAddress(addressDto.getFullAddress());
-            employeeDto.setAddressDetails(addressDto);
-            employeeDto.setPhotoUrl(externalData.getPicture().toString());
-            employeeDto.setPhone(externalData.getPhone());
-            employeeDto.setWorkPhone(externalData.getCell());
-            employeeDto.setBirthDate(LocalDate.parse(externalData.getDob().getDate()));
-            employeeDto.setUsername(externalData.getLogin().getUsername());
-            employeeDto.setCreationDate(ZonedDateTime.parse(externalData.getRegistered().getDate()));
-            employeeDtoList.add(employeeDto);
-        }
-        return employeeDtoList;
+        return externalDataList.stream()
+                .map(externalData -> {
+                    ExternalData.Location location = externalData.getLocation();
+                    ExternalData.Name name = externalData.getName();
+                    String externalId = String.valueOf(externalData.getId());
+                    AddressDto addressDto = new AddressDto();
+                    addressDto.setFullAddress(location.toString());
+                    addressDto.setStreet(location.getStreet().getName());
+                    addressDto.setHouse(location.getStreet().getNumber());
+                    addressDto.setIndex(location.getPostcode());
+                    addressDto.setCity(location.getCity());
+                    addressDto.setRegion(location.getState());
+                    addressDto.setCountry(location.getCountry());
+                    EmployeeDto employeeDto = new EmployeeDto();
+                    employeeDto.setExternalId(externalId);
+                    employeeDto.setFirstName(name.getFirst());
+                    employeeDto.setLastName(name.getLast());
+                    employeeDto.setMiddleName(name.getMiddle());
+                    employeeDto.setAddress(addressDto.getFullAddress());
+                    employeeDto.setAddressDetails(addressDto);
+                    employeeDto.setPhotoUrl(externalData.getPicture().toString());
+                    employeeDto.setPhone(externalData.getPhone());
+                    employeeDto.setWorkPhone(externalData.getCell());
+                    employeeDto.setBirthDate(LocalDate.parse(externalData.getDob().getDate()));
+                    employeeDto.setUsername(externalData.getLogin().getUsername());
+                    employeeDto.setCreationDate(ZonedDateTime.parse(externalData.getRegistered().getDate()));
+                    return employeeDto;
+                })
+                .collect(Collectors.toList());
     }
 
     /**
@@ -125,26 +117,27 @@ public class DataFetchingService {
      * @return Список объектов DepartmentDto.
      */
     private List<DepartmentDto> mapToDepartmentDto(List<ExternalData> externalDataList) {
-        Set<DepartmentDto> departmentDtoSet = new HashSet<>();
-        for (ExternalData externalData : externalDataList) {
-            ExternalData.Company company = externalData.getCompany();
-            String externalId = String.valueOf(company.getId());
-            AddressDto addressDto = new AddressDto();
-            addressDto.setFullAddress(company.getLocation().toString());
-            addressDto.setStreet(company.getLocation().getStreet().getName());
-            addressDto.setHouse(company.getLocation().getStreet().getNumber());
-            addressDto.setIndex(company.getLocation().getPostcode());
-            addressDto.setCity(company.getLocation().getCity());
-            addressDto.setRegion(company.getLocation().getState());
-            addressDto.setCountry(company.getLocation().getCountry());
-            DepartmentDto departmentDto = new DepartmentDto();
-            departmentDto.setExternalId(externalId);
-            departmentDto.setFullName(company.getName());
-            departmentDto.setAddress(addressDto.getFullAddress());
-            departmentDto.setAddressDetails(addressDto);
-            departmentDtoSet.add(departmentDto);
-        }
-        return new ArrayList<>(departmentDtoSet);
+        return externalDataList.stream()
+                .map(externalData -> {
+                    ExternalData.Company company = externalData.getCompany();
+                    String externalId = String.valueOf(company.getId());
+                    AddressDto addressDto = new AddressDto();
+                    addressDto.setFullAddress(company.getLocation().toString());
+                    addressDto.setStreet(company.getLocation().getStreet().getName());
+                    addressDto.setHouse(company.getLocation().getStreet().getNumber());
+                    addressDto.setIndex(company.getLocation().getPostcode());
+                    addressDto.setCity(company.getLocation().getCity());
+                    addressDto.setRegion(company.getLocation().getState());
+                    addressDto.setCountry(company.getLocation().getCountry());
+                    DepartmentDto departmentDto = new DepartmentDto();
+                    departmentDto.setExternalId(externalId);
+                    departmentDto.setFullName(company.getName());
+                    departmentDto.setAddress(addressDto.getFullAddress());
+                    departmentDto.setAddressDetails(addressDto);
+                    return departmentDto;
+                })
+                .distinct()
+                .collect(Collectors.toList());
     }
 
     /**
