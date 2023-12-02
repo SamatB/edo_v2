23a2dca65ee3d45ebeca2ce9174f7ae3d5503687
Ann S <404region@gmail.com>
2023-12-02 19:56:32 +0300

    #59 Добавлен Класс, представляющий Листа согласования. Добавлены классы Dto, Mapper, Repository, Миграция

---
+++ edo-common/src/main/java/org/example/dto/AgreementListDto.java
@@ -0,0 +1,49 @@
+package org.example.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.ZonedDateTime;
+
+/**
+ * DTO-класс данных Листа согласования
+ */
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Schema(description = "Лист согласования")
+public class AgreementListDto {
+    @NotNull
+    @Schema(description = "Дата создания")
+    private ZonedDateTime creationDate;
+
+    @Schema(description = "Дата направления на согласование")
+    private ZonedDateTime sentApprovalDate;
+
+    @Schema(description = "Дата подписания")
+    private ZonedDateTime signDate;
+
+    @Schema(description = "Дата возврата на доработку")
+    private ZonedDateTime returnedDate;
+
+
+    @Schema(description = "Дата обработки возврата")
+    private ZonedDateTime refundProcessingDate;
+
+
+    @Schema(description = "Дата архивности")
+    private ZonedDateTime archiveDate;
+
+    @Schema(description = "Комментарий инициатора")
+    private String comment;
+
+    @Schema(description = "Обращение с которым связан лист согласования")
+    private AppealDto appealDto;
+}
+
+
