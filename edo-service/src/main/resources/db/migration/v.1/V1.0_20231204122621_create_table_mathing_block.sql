b2ea73497721691d465df230dfbb5be9e7689f34
Максим Беспалов <maksim.bespalov@domrf.ru>
2024-01-16 20:14:51 +0500

    return migration changes

--- edo-service/src/main/resources/db/migration/v.1/V1.0_20231204122621_create_table_mathing_block.sql
+++ edo-service/src/main/resources/db/migration/v.1/V1.0_20231204122621_create_table_mathing_block.sql
@@ -4,8 +4,7 @@ CREATE TABLE IF NOT EXISTS matching_block(
     number         BIGINT NOT NULL,
     type           TEXT NOT NULL
     );
+
 comment on column matching_block.id is 'Идентификатор блока согласования';
 comment on column matching_block.number is 'Номер согласования';
 comment on column matching_block.type is 'Тип согласования';
-
-
