781850edfbb99732204e45974923e12a32ac8591
Nikolaev Andrey <kisamasyanya@gmail.com>
2024-02-02 11:02:50 +0300

    -

--- edo-service/src/main/resources/db/migration/v.1/V1.0_20240202103000__add_columns_to_appeal.sql
+++ edo-service/src/main/resources/db/migration/v.1/V1.0_20240202103000__add_columns_to_appeal.sql
@@ -1,3 +1,3 @@
 -- Добавление колонок registration_date и appeal_status
-ALTER TABLE appeal ADD COLUMN registration_date TIMESTAMP;
+ALTER TABLE appeal ADD COLUMN registration_date TIMESTAMP WITH TIME ZONE;
 ALTER TABLE appeal ADD COLUMN appeal_status VARCHAR(15);
