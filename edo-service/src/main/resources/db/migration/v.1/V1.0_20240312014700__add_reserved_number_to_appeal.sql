8e93ec21dfba2a42c8ad3d118a22022a0b290a8d
Yvette Demidova <yvette_demidova@mail.ru>
2024-03-14 08:47:25 +0300

    Changed Appeal and AppealDto to change NotNull constraints on fields "number" and "nomenclature" with DB migration

--- edo-service/src/main/resources/db/migration/v.1/V1.0_20240312014700__add_reserved_number_to_appeal.sql
+++ edo-service/src/main/resources/db/migration/v.1/V1.0_20240312014700__add_reserved_number_to_appeal.sql
@@ -1,2 +1,8 @@
--- Добавление колонок registration_date и appeal_status
-ALTER TABLE IF EXISTS appeal ADD COLUMN IF NOT EXISTS reserved_number VARCHAR(50);
+-- Добавление колонки reserved_number
+-- удаление ограничения NOT NULL для столбца number
+-- установка ограничения NOT NULL для столбца nomenclature
+
+ALTER TABLE IF EXISTS appeal
+    ADD COLUMN IF NOT EXISTS reserved_number VARCHAR(50),
+    ALTER COLUMN number DROP NOT NULL,
+    ALTER COLUMN nomenclature_id SET NOT NULL;
