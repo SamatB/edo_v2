0db7f549418e54c634cebc4214723be935ad6a2c
Yvette Demidova <yvette_demidova@mail.ru>
2024-03-17 20:10:49 +0300

    In edo-main: Changed AppealFeignClient: added new method for downloading xlsx report and getting all Appeals from database. Changed AppealController: added new methods for downloading Appeal report in xlsx format and getting all Appeals from database. Added more tests for AppealController.

--- edo-main/src/main/java/org/example/feign/AppealFeignClient.java
+++ edo-main/src/main/java/org/example/feign/AppealFeignClient.java
@@ -5,11 +5,14 @@ package org.example.feign;
 
 import org.example.dto.AppealDto;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
 
 @FeignClient(name = "edo-service")
 public interface AppealFeignClient {
@@ -37,4 +40,16 @@ public interface AppealFeignClient {
      */
     @PostMapping("/appeal/{id}/register")
     AppealDto registerAppeal(@PathVariable Long id);
+
+    /**
+     * Метод для получения списка обращений
+     */
+    @GetMapping("/appeal")
+    List<AppealDto> getAllAppeals();
+
+    /**
+     * Метод для получения списка обращений в виде XLSX
+     */
+    @GetMapping(value="/appeal/export/excel", consumes = "application/octet-stream", produces = MediaType.APPLICATION_OCTET_STREAM_VALUE)
+    byte[] downloadAppealsXlsxReport();
 }
