fec65b4d1579f23e39dd06843b7a7020793b521f
Yvette Demidova <yvette_demidova@mail.ru>
2024-03-17 20:11:27 +0300

    Cleaned up unused imports

--- edo-service/src/main/java/org/example/service/impl/ReportServiceImpl.java
+++ edo-service/src/main/java/org/example/service/impl/ReportServiceImpl.java
@@ -1,6 +1,5 @@
 package org.example.service.impl;
 
-import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.example.entity.Appeal;
