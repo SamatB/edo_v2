e47b4d08e965f8fef277549802acc66ba205dec8
Yvette Demidova <yvette_demidova@mail.ru>
2024-03-17 20:10:49 +0300

    In edo-main: Changed AppealFeignClient: added new method for downloading xlsx report and getting all Appeals from database. Changed AppealController: added new methods for downloading Appeal report in xlsx format and getting all Appeals from database. Added more tests for AppealController.

--- edo-main/src/test/java/org/example/controller/AppealControllerTest.java
+++ edo-main/src/test/java/org/example/controller/AppealControllerTest.java
@@ -1,6 +1,7 @@
 package org.example.controller;
 
 import feign.FeignException;
+import org.apache.commons.io.FileUtils;
 import org.example.dto.AppealDto;
 import org.example.feign.AppealFeignClient;
 import org.junit.jupiter.api.BeforeEach;
@@ -11,6 +12,15 @@ import org.mockito.MockitoAnnotations;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.*;
@@ -31,6 +41,8 @@ public class AppealControllerTest {
 
     @Mock
     FeignException.BadRequest badRequestException;
+
+    private static final String OUTPUT_DIR = "src/test/resources/";
 
     @BeforeEach
     public void setUp() {
@@ -213,5 +225,112 @@ public class AppealControllerTest {
         when(appealFeignClient.reserveNumberForAppeal(appealDto)).thenThrow(badRequestException);
         ResponseEntity<AppealDto> response = appealController.reserveNumberForAppeal(appealDto);
         assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+    }
+
+    /**
+     * Тест для метода getAllAppeals.
+     * Возвращает список обращений со статусом 200.
+     */
+    @Test
+    void getAllAppeals_returnsOk() {
+        AppealDto appealDto1 = new AppealDto();
+        AppealDto appealDto2 = new AppealDto();
+        List<AppealDto> appealDtos = new ArrayList<>(Arrays.asList(appealDto1, appealDto2));
+        when(appealFeignClient.getAllAppeals()).thenReturn(appealDtos);
+        ResponseEntity<?> response = appealController.getAllAppeals();
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals(appealDtos, response.getBody());
+    }
+
+    /**
+     * Тест для метода getAllAppeals.
+     * Возвращает NoContent если список обращений пуст (со статусом 204).
+     */
+    @Test
+    void getAllAppeals_returnsNoContent() {
+        List<AppealDto> appealDtos = new ArrayList<>();
+        when(appealFeignClient.getAllAppeals()).thenReturn(appealDtos);
+        ResponseEntity<?> response = appealController.getAllAppeals();
+
+        assertEquals(HttpStatus.NO_CONTENT, response.getStatusCode());
+        assertNull(response.getBody());
+    }
+
+    /**
+     * Тест для метода downloadAppealsXlsxReport.
+     * Возвращает статус 200, если файл был получен.
+     */
+    @Test
+    void downloadAppealsXlsxReport_returnsOk() throws IOException {
+        File testXlsx = new File(OUTPUT_DIR + "appeals_2024-03-17_16_03_38.xlsx");
+        ByteArrayInputStream mockInputStream = new ByteArrayInputStream(FileUtils.readFileToByteArray(testXlsx));
+        byte[] bytes = mockInputStream.readAllBytes();
+
+        when(appealFeignClient.downloadAppealsXlsxReport()).thenReturn(bytes);
+
+        ResponseEntity<?> response = appealController.getAllAppealsAsXlsx();
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+    }
+
+    /**
+     * Тест для метода downloadAppealsXlsxReport.*
+     * Проверяет, что тело ответа содержит правильную длину
+     */
+    @Test
+    void downloadAppealsXlsxReport_responseHasBodyWithCorrectContentLength() throws IOException {
+        File testXlsx = new File(OUTPUT_DIR + "appeals_2024-03-17_16_03_38.xlsx");
+        ByteArrayInputStream mockInputStream = new ByteArrayInputStream(FileUtils.readFileToByteArray(testXlsx));
+        int size = mockInputStream.available();
+        byte[] bytes = mockInputStream.readAllBytes();
+
+        when(appealFeignClient.downloadAppealsXlsxReport()).thenReturn(bytes);
+
+        ResponseEntity<?> response = appealController.getAllAppealsAsXlsx();
+        assertEquals(size, response.getHeaders().getContentLength());
+        assertThat(response.getBody()).isNotNull();
+    }
+
+    /**
+     * Тест для метода downloadAppealsXlsxReport.
+     * Проверяет формат имени файла
+     */
+    @Test
+    void downloadAppealsXlsxReport_fileHasSuffix() throws IOException {
+        File testXlsx = new File(OUTPUT_DIR + "appeals_2024-03-17_16_03_38.xlsx");
+        ByteArrayInputStream mockInputStream = new ByteArrayInputStream(FileUtils.readFileToByteArray(testXlsx));
+        when(appealFeignClient.downloadAppealsXlsxReport()).thenReturn(mockInputStream.readAllBytes());
+
+        ResponseEntity<?> response = appealController.getAllAppealsAsXlsx();
+
+        assertThat(response.getHeaders().containsKey("Content-Disposition")).isTrue();
+        assertThat(Objects.requireNonNull(response.getHeaders().get("Content-Disposition")).get(0)).endsWithIgnoringCase(".xlsx");
+        assertThat(Objects.requireNonNull(response.getHeaders().get("Content-Disposition")).get(0)).startsWithIgnoringCase("attachment; filename=");
+    }
+
+    /**
+     * Тест для метода downloadAppealsXlsxReport.
+     * Проверяет, что в случае ошибки открытия потока ввода данных, возвращается статус 500
+     */
+    @Test
+    void downloadAppealsXlsxReport_returnsInternalServerError() {
+        when(appealFeignClient.downloadAppealsXlsxReport()).thenReturn(null);
+
+        ResponseEntity<?> response = appealController.getAllAppealsAsXlsx();
+
+        assertEquals(HttpStatus.INTERNAL_SERVER_ERROR, response.getStatusCode());
+    }
+
+    /**
+     * Тест для метода downloadAppealsXlsxReport.
+     * Возвращает статус 404, если получен файл нулевой длины
+     */
+    @Test
+    void downloadAppealsXlsxReport_returnsNotFound() {
+        ByteArrayInputStream mockInputStream = new ByteArrayInputStream(new byte[0]);
+        when(appealFeignClient.downloadAppealsXlsxReport()).thenReturn(mockInputStream.readAllBytes());
+
+        ResponseEntity<?> response = appealController.getAllAppealsAsXlsx();
+
+        assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
     }
 }
