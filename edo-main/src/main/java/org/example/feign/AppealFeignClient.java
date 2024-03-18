1acc638c02231bfccf58ece39f258abfbe069e26
Yvette Demidova <yvette_demidova@mail.ru>
2024-03-18 21:32:02 +0300

    Changed in edo-main AppealFeignClient, AppealController: added method for downloading appeals csv report. Added tests for controller with file as test resource.

--- edo-main/src/main/java/org/example/feign/AppealFeignClient.java
+++ edo-main/src/main/java/org/example/feign/AppealFeignClient.java
@@ -5,6 +5,7 @@ package org.example.feign;
 
 import org.example.dto.AppealDto;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -46,4 +47,10 @@ public interface AppealFeignClient {
      */
     @PostMapping("/appeal/reserve-number")
     AppealDto reserveNumberForAppeal(@RequestBody AppealDto appeal);
+
+    /**
+     * Метод для выгрузки обращений в формате CSV
+     */
+    @GetMapping(value = "/appeal/export/csv", consumes = "application/octet-stream", produces = MediaType.APPLICATION_OCTET_STREAM_VALUE)
+    byte[] downloadAppealsCsvReport();
 }
