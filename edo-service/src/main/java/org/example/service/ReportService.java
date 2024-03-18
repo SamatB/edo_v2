9b6f623416d383d9a3cd506dca72648a25de0370
Yvette Demidova <yvette_demidova@mail.ru>
2024-03-18 21:37:23 +0300

    Changed in edo-service - AppealController: added method for downloading appeals csv report. - ReportService: added method for downloading appeals csv report - ReportServiceImpl: added method for downloading appeals csv report, refactored method for saving appeals csv report to file

--- edo-service/src/main/java/org/example/service/ReportService.java
+++ edo-service/src/main/java/org/example/service/ReportService.java
@@ -1,5 +1,8 @@
 package org.example.service;
 
+import java.io.ByteArrayInputStream;
+
 public interface ReportService {
     void writeAppealsToCsv(String fileName);
+    ByteArrayInputStream downloadAppealsCsvReport();
 }
