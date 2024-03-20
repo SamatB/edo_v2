cd2737c0e50b3e5b3fccb9e748d1c94d4f0b3087
Yvette Demidova <yvette_demidova@mail.ru>
2024-03-20 13:36:07 +0300

    Added EmailDto in common and EmailHelper for creating agreement list email notification

--- edo-common/src/main/java/org/example/dto/EmailDto.java
+++ edo-common/src/main/java/org/example/dto/EmailDto.java
@@ -1,6 +1,13 @@
 package org.example.dto;
 
 import lombok.Data;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
 
 import java.util.List;
 
@@ -11,10 +18,26 @@ import java.util.List;
  * через систему или внешние сервисы.
  ***/
 @Data
-public class EmailDto {
+@NoArgsConstructor
+@AllArgsConstructor
+@Schema(description = "Email")
+public class EmailDto implements Serializable {
 
+    @Schema(description="Список адресов для рассылки")
     private List<String> email;
-    private String subject;
-    private String text;
 
+    @Schema(description = "Идентификатор получателя")
+    private Long employeeId;
+
+    @Schema(description = "Адрес электронной почты отправителя")
+    private String from;
+
+    @Schema(description = "Адрес электронной почты получателя")
+    private String to;
+
+    @Schema(description = "Тема письма")
+    private String subject;
+
+    @Schema(description = "Тело письма")
+    private String body;
 }
