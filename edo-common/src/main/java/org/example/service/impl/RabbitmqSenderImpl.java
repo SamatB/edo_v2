7c5f7c7c1db1d88a2d738233faec980ae4fbdc60
Yvette Demidova <yvette_demidova@mail.ru>
2024-03-20 14:10:26 +0300

    Changed RabbitMQ configuration and sender to implement priority queue for sending agreement list to RabbitMQ queue for email service

--- edo-common/src/main/java/org/example/service/impl/RabbitmqSenderImpl.java
+++ edo-common/src/main/java/org/example/service/impl/RabbitmqSenderImpl.java
@@ -6,6 +6,7 @@ import lombok.Setter;
 import org.example.service.RabbitmqSender;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.amqp.core.MessagePostProcessor;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Service;
 
@@ -19,10 +20,20 @@ import org.springframework.stereotype.Service;
 public class RabbitmqSenderImpl implements RabbitmqSender {
     private final Logger logger = LoggerFactory.getLogger(RabbitmqSenderImpl.class);
     private final RabbitTemplate rabbitTemplate;
+
     @Override
     public void send(String queue, Object dto) {
         try {
             rabbitTemplate.convertAndSend(queue, dto);
+            logger.info("Sending Message to the Queue : " + queue);
+        } catch (Exception e) {
+            System.out.println("Sending error Message to the Queue : " + e.getMessage());
+        }
+    }
+
+    public void send(String queue, Object dto, MessagePostProcessor messagePostProcessor) {
+        try {
+            rabbitTemplate.convertAndSend(queue, dto, messagePostProcessor);
             logger.info("Sending Message to the Queue : " + queue);
         } catch (Exception e) {
             System.out.println("Sending error Message to the Queue : " + e.getMessage());
---
+++ edo-service/src/main/java/org/example/publisher/AgreementListPublisher.java
@@ -0,0 +1,40 @@
+package org.example.publisher;
+
+import lombok.RequiredArgsConstructor;
+import org.example.dto.EmailDto;
+import org.example.service.RabbitmqSender;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+/**
+ * Класс публикует в очередь RabbitMQ листа согласования
+ */
+
+@Component
+@RequiredArgsConstructor
+public class AgreementListPublisher {
+    private final RabbitmqSender rabbitmqSender;
+    public static final String SEND_AGREEMENT_LIST_ROUTING_KEY = "sendAgreementList";
+
+    public void sendAgreementListEmailNotification(EmailDto emailDto, Long priority) {
+        rabbitmqSender.send(SEND_AGREEMENT_LIST_ROUTING_KEY, emailDto, message -> {
+            message.getMessageProperties().setPriority(getPriority(priority));
+            return message;
+        });
+    }
+
+    /**
+     * Метод принимает номер участника и преобразует его в приоритет в очереди
+     * @param priority номер участника
+     * @return приоритет в очереди
+     */
+    private Integer getPriority(Long priority) {
+        if (priority > 255) {
+            return 1;
+        } else if (priority < 1) {
+            return 0;
+        } else {
+            return 256 - priority.intValue();
+        }
+    }
+}
