0e4e3044ff905a150e1de270daa3a45ab085b6b2
Yvette Demidova <yvette_demidova@mail.ru>
2024-03-21 16:04:43 +0300

    Clean-up unused imports in EmailDto

--- edo-common/src/main/java/org/example/dto/EmailDto.java
+++ edo-common/src/main/java/org/example/dto/EmailDto.java
@@ -3,9 +3,7 @@ package org.example.dto;
 import lombok.Data;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 import java.io.Serializable;
 
