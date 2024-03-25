d18821f1660797b1c727e27da7cd4ccec36a75ef
Yvette Demidova <yvette_demidova@mail.ru>
2024-03-25 03:42:31 +0300

    Refactoring FileHelper: changer constants naming according to standards. Moved more constants from AppealCsvHelper to FileHelper. Solved getting questions from AppealDto.

--- edo-common/src/main/java/org/example/utils/FileHelper.java
+++ edo-common/src/main/java/org/example/utils/FileHelper.java
@@ -3,18 +3,22 @@ package org.example.utils;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.time.format.DateTimeFormatter;
+import java.util.Arrays;
 import java.util.Date;
+import java.util.List;
 
 /**
  * Утилитный класс для формирования имени файлов.
  */
 public class FileHelper {
     public static final String CSV_FILE_EXTENSION = ".csv";
-    public static final DateTimeFormatter dateNoTimeFormatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-    public static final DateFormat dateTimeWithSecondsFormatter = new SimpleDateFormat("yyyy-MM-dd_HH:mm:ss");
+    public static final List<String> APPEAL_REPORT_COLUMNS = Arrays.asList("Номер обращения", "Дата создания", "ФИО создателя", "Список вопросов обращения", "Статус обращения");
+    public static final String COMMA_DELIMITER = ", ";
+    public static final DateTimeFormatter DAY_FIRST_DATE_NO_TIME_FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    public static final DateFormat DATE_TIME_WITH_SECONDS_FORMATTER = new SimpleDateFormat("yyyy-MM-dd_HH:mm:ss");
 
     public static String getAppealsCsvReportFileName() {
-        String currentDateTime = dateTimeWithSecondsFormatter.format(new Date());
+        String currentDateTime = DATE_TIME_WITH_SECONDS_FORMATTER.format(new Date());
         return "appeals_" +
                 currentDateTime +
                 CSV_FILE_EXTENSION;
