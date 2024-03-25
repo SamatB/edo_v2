4292bcf4b035987056d805a1f9488065d48e4a6a
Yvette Demidova <yvette_demidova@mail.ru>
2024-03-25 03:35:30 +0300

    Added questions field to AppealDto. Fixed naming in QuestionDto according to standards. Reworked QuestionMapper to support mapping Appeal from appealId. Added AppealMapperResolver for getting Appeal from AppealRepository when mapping from appealId. Added tests for QuestionMapper and AppealMapper. Added db migration script for linking questions to Appeal and test data for it.

--- edo-service/src/main/java/org/example/mapper/QuestionMapper.java
+++ edo-service/src/main/java/org/example/mapper/QuestionMapper.java
@@ -4,10 +4,18 @@ import org.example.dto.QuestionDto;
 import org.example.entity.Question;
 import org.example.mapper.util.AbstractMapper;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 /**
  * Маппер преобразует Question в QuestionDto и обратно
  */
-@Mapper(componentModel = "spring", uses = {AppealMapper.class})
+@Mapper(componentModel = "spring", uses = {AppealMapperResolver.class})
 public interface QuestionMapper extends AbstractMapper<Question, QuestionDto> {
+    @Override
+    @Mapping(target = "appealId", source = "appeal.id")
+    QuestionDto entityToDto(Question question);
+
+    @Override
+    @Mapping(source = "appealId", target = "appeal")
+    Question dtoToEntity(QuestionDto questionDto);
 }
