7b211608a1d8a7131ec53aa66403a1123a3c7628
ravil <ravilshulayakov@gmail.com>
2024-04-01 13:29:16 +0300

    Rework 1.0

--- edo-service/src/main/resources/db/migration/v.2/V2.0_20240328141300__insert_test_data_to_question.sql
+++ edo-service/src/main/resources/db/migration/v.2/V2.0_20240328141300__insert_test_data_to_question.sql
@@ -3,3 +3,5 @@ SELECT now(),
        'REGISTERED',
        'test question',
        1 WHERE EXISTS (SELECT 1 FROM appeal WHERE id = 1);
+-- WHERE EXISTS проверяет есть ли в таблице appeal id = 1 --
+-- Если нету, то вставка всей строки игнорируется --
--- edo-service/src/main/resources/db/migration/v.2/V2.0_20240328142600__insert_test_data_to_resolution_executor.sql
+++ edo-service/src/main/resources/db/migration/v.2/V2.0_20240328142600__insert_test_data_to_resolution_executor.sql
@@ -1,11 +1,11 @@
-INSERT INTO resolution_executor (resolution_id, employee_id)
+INSERT INTO resolution_executor (employee_id, resolution_id)
 SELECT 1,
        1 WHERE EXISTS (SELECT 1 FROM resolution WHERE id = 1);
 ----------------------------------------------------------------------------------------------
-INSERT INTO resolution_executor (resolution_id, employee_id)
+INSERT INTO resolution_executor (employee_id, resolution_id)
 SELECT 2,
        2 WHERE EXISTS (SELECT 1 FROM resolution WHERE id = 2);
 ----------------------------------------------------------------------------------------------
-INSERT INTO resolution_executor (resolution_id, employee_id)
+INSERT INTO resolution_executor (employee_id, resolution_id)
 SELECT 3,
        3 WHERE EXISTS (SELECT 1 FROM resolution WHERE id = 3);
